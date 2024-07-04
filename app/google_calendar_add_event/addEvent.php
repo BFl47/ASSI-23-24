b9cef2521ba7173948f99b844c1b3fb9237b9deb
Benny <bennyf@live.it>
2024-07-04 16:52:59 +0200

    inserimento corso (ruolo admin) terminato

--- app/google_calendar_add_event/addEvent.php
+++ app/google_calendar_add_event/addEvent.php
@@ -15,10 +15,18 @@
         $data = $_POST['dataevento']; 
         $time_from = $_POST['time_from']; 
         $time_to = $_POST['time_to']; 
+        $occorrenze = $_POST['terminadopo'];
+
+        $giorni = [];
+        if (isset($_POST['days'])) {
+            $giorni = $_POST['days'];
+        }
+
+        $rrule = "RRULE:FREQ=WEEKLY;BYDAY=" . implode(',', $giorni) . ";COUNT=" . intval($occorrenze);
 
         $event_id = uniqid();
-        $q = "INSERT into corso values ($1,$2,$3, $4,$5, $6,$7, $8)";               
-        $data = pg_query_params($dbconn, $q, array($event_id, $nome, $descrizione, $luogo, $data, $time_from, $time_to, $trainer));
+        $q = "INSERT into corso values ($1,$2,$3, $4,$5, $6,$7, $8, $9)";               
+        $data = pg_query_params($dbconn, $q, array($event_id, $nome, $descrizione, $luogo, $data, $time_from, $time_to, $trainer, $rrule));
   
         $_SESSION['last_event_id'] = $event_id; 
                 
