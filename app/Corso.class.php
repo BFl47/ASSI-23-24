be93be0d90178b3a99582f697231e9489a9eb531
Benny <bennyf@live.it>
2024-07-10 18:02:30 +0200

    test unità Corso

---
+++ app/Corso.class.php
@@ -0,0 +1,73 @@
+<?php
+class Corso {
+    private $id;
+    private $nome;
+    private $descrizione;
+    private $luogo;
+    private $data;
+    private $time_from;
+    private $time_to;
+    private $trainer;
+    private $rrule;
+    private $id_google;
+
+    // Costruttore
+    public function __construct($id, $nome, $descrizione, $luogo, $data, $time_from, $time_to, $trainer, $rrule, $id_google = null) {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->descrizione = $descrizione;
+        $this->luogo = $luogo;
+        $this->data = $data;
+        $this->time_from = $time_from;
+        $this->time_to = $time_to;
+        $this->trainer = $trainer;
+        $this->rrule = $rrule;
+        $this->id_google = $id_google;
+    }
+
+    // Metodi getter
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getDescrizione() {
+        return $this->descrizione;
+    }
+
+    public function getLuogo() {
+        return $this->luogo;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function getTimeFrom() {
+        return $this->time_from;
+    }
+
+    public function getTimeTo() {
+        return $this->time_to;
+    }
+
+    public function getTrainer() {
+        return $this->trainer;
+    }
+
+    public function getRrule() {
+        return $this->rrule;
+    }
+
+    public function getIdGoogle() {
+        return $this->id_google;
+    }
+
+    public function setIdGoogle($id_google) {
+        $this->id_google = $id_google;
+    }
+}
+?>
--- app/google_calendar_add_event/addEvent.php
+++ app/google_calendar_add_event/addEvent.php
@@ -1,5 +1,6 @@
 <?php     
     require_once 'config.php'; 
+    include_once '../Corso.class.php'; 
 
     $dbconn = pg_connect("host=localhost port=5432 dbname=GymGeniusASSI user=postgres password=password") 
         or die('Could not connect: ' . pg_last_error());
@@ -27,6 +28,10 @@
         $event_id = uniqid();
         $q = "INSERT into corso values ($1,$2,$3, $4,$5, $6,$7, $8, $9)";               
         $data = pg_query_params($dbconn, $q, array($event_id, $nome, $descrizione, $luogo, $data, $time_from, $time_to, $trainer, $rrule));
+
+        $corso = new Corso($event_id, $nome, $descrizione, $luogo, $data, $time_from, $time_to, $trainer, $rrule);
+        $_SESSION['corso'] = $corso;
+
         $_SESSION['last_event_id'] = $event_id;
         
 
