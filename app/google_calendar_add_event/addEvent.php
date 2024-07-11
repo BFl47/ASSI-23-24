4f7ce55c21e1dfee40f95af72290abf1737e6228
Benny <bennyf@live.it>
2024-07-11 11:15:00 +0200

    correzione bug

--- app/google_calendar_add_event/addEvent.php
+++ app/google_calendar_add_event/addEvent.php
@@ -29,9 +29,6 @@
         $q = "INSERT into corso values ($1,$2,$3, $4,$5, $6,$7, $8, $9)";               
         $data = pg_query_params($dbconn, $q, array($event_id, $nome, $descrizione, $luogo, $data, $time_from, $time_to, $trainer, $rrule));
 
-        $corso = new Corso($event_id, $nome, $descrizione, $luogo, $data, $time_from, $time_to, $trainer, $rrule);
-        $_SESSION['corso'] = $corso;
-
         $_SESSION['last_event_id'] = $event_id;
         
 
